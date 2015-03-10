d15af1fc958259c9b484bea6e06b1bb72aac275b
aliaksey <aliaksey.vasilevich@gmail.com>
2015-03-10 12:56:23 +0100

    introduced new variables for focus

--- icam_load_data_filter_in_focus.R
+++ icam_load_data_filter_in_focus.R
@@ -1,13 +1,13 @@
 trval<-0.29 #set treshhold value for posotive cells
 load("data/icamimageandobjectindex.RData")
-perimindallf<-perimindall[,c("ImageNumber", "Image_ImageQuality_LocalFocusScore_Actin1Crop_40",
-                             "Image_ImageQuality_LocalFocusScore_Dapi1Crop_40",
-                             "Image_ImageQuality_LocalFocusScore_Icam1Crop_40",
-                             "Image_ImageQuality_FocusScore_Actin1Crop",                  
-                             "Image_ImageQuality_FocusScore_Dapi1Crop",                   
-                             "Image_ImageQuality_FocusScore_Icam1Crop",
-                             "FeatureIdx","InternalIdx","Image_Math_focusa",                                         
-                             "Image_Math_focusd","Image_Metadata_array" ),]
+#selecting only variables that dealing with focus
+
+perimindallf<-perimindall[,c("ImageNumber","FeatureIdx","InternalIdx","Image_Metadata_array",
+              colnames(perimindall)[grepl("Focus",colnames(perimindall))],
+              colnames(perimindall)[grepl("focus",colnames(perimindall))],
+              colnames(perimindall)[grepl("LogLogSlope",colnames(perimindall))])]
+LogLogSlope     
+
 perimindallftt<-perimindall[perimindall$Image_Metadata_array>8,]
 
 
@@ -27,25 +27,25 @@ p11+ geom_point(alpha = 0.4) +geom_density2d()+xlab("log_actin_local_focus_meas"
   ylab("log_dapi_local_focus_meas")+ geom_segment(aes(x = -4.2, y = -5, xend = -4.2, yend = 2, colour="red"))+
   geom_segment(aes(x = -6, y = -2.5, xend = 1, yend = -2.5, colour="red"))
 
-+geom_density2d()+
-  ylim(0,0.1)+xlim(0,0.0025)
+# +geom_density2d()+
+#   ylim(0,0.1)+xlim(0,0.0025)
 
-
+library(hexbin)
 p11+stat_binhex()
 p11+geom_point(alpha = 0.4)
 p11 + geom_point() + geom_density2d()
-p11+ geom_point(size = 3, aes(colour = highlight)) +
-  scale_color_manual("Status", values = mycolours) +
-  geom_text(data = textdf, aes(x = Index1, y = Index2, label = "blank")) +
-  theme(legend.position = "none") +
-  theme()
+# p11+ geom_point(size = 3, aes(colour = highlight)) +
+#   scale_color_manual("Status", values = mycolours) +
+#   geom_text(data = textdf, aes(x = Index1, y = Index2, label = "blank")) +
+#   theme(legend.position = "none") +
+#   theme()
 
 focim<-perimindallf[perimindallf$Image_Math_focusa>-4.2&perimindallf$Image_Math_focusd>-2.5,"ImageNumber"]
 # finding images without over exposion 
 perimindalltt<-perimindall[perimindall$Image_Metadata_array<9,]
-p11<-ggplot(data = perimindalltt, aes(x = Image_ImageQuality_MinIntensity_Icam1Crop,
+p12<-ggplot(data = perimindalltt, aes(x = Image_ImageQuality_MinIntensity_Icam1Crop,
                                       y = Image_ImageQuality_MedianIntensity_Icam1Crop)) 
-p11+ geom_point(alpha = 0.4) +geom_density2d()
+p12+ geom_point(alpha = 0.4) +geom_density2d()
 
 corrimt<-perimindall[perimindall$Image_ImageQuality_MinIntensity_Icam1Crop<0.28&
                        perimindall$Image_ImageQuality_MedianIntensity_Icam1Crop<0.325&
