80c9c4a8b756f9692c27db4ea0cd130182533fc9
aliaksey <aliaksey.vasilevich@gmail.com>
2015-03-17 11:41:00 +0100

    all

--- icam_run_all.R
+++ icam_run_all.R
@@ -1,3 +1,6 @@
 ##load data and filter images that are out of focus and have two high min intensity
 source("icam_load_data_filter_in_focus.R")
 ##filtering cells based on area aamd perimeter
+source("icam_filter_cells.R")
+##correct for intensities in different repeats
+source("")
